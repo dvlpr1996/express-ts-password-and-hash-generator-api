e777fc6c83912bf1ee664cd1bcf76ff84521c253
dvlpr1996 <nimajahanbakhshian@gmail.com>
2024-11-30 22:54:59 +0330

    implement constants for password generator

---
+++ src/config/constant/constants.ts
@@ -0,0 +1,5 @@
+export const UPPERCASE_CHARS: String = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ';
+export const LOWERCASE_CHARS: String = 'abcdefghijklmnopqrstuvwxyz';
+export const NUMERIC_CHARS: String = '0123456789';
+export const SPECIAL_CHARS: String = '!@#$%^&*()-_=+[]{}|;:,.<>?';
+export const SIMILAR_CHARS = /[Il1Os5z2@a0gqb96o`'lS5Z2@a]/g;
