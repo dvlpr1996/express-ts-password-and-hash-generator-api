bceb2c32dfa6ad255409fcea95440507db540e6b
dvlpr1996 <nimajahanbakhshian@gmail.com>
2024-12-03 22:39:38 +0330

    implement limitRequestBodyKeys middleware

---
+++ src/middlewares/limitRequestBodyKeys.ts
@@ -0,0 +1,14 @@
+import { NextFunction, Request, Response } from 'express';
+
+const limitRequestBodyKeys = (maxKeys: number) => {
+  return (req: Request, res: Response, next: NextFunction) => {
+    const bodyKeysCount = Object.keys(req.body).length;
+    if (bodyKeysCount > maxKeys) {
+      res.status(400).json(`Request body contains too many keys. Maximum allowed is ${maxKeys}`);
+      return;
+    }
+    next();
+  };
+};
+
+export default limitRequestBodyKeys;
